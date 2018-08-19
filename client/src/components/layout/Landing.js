9f97196668a4a9f66d275b38103e05c9f2710a8a
munozgonzaloa <munozgonzaloa@gmail.com>
2018-08-19 14:53:10 -0300

    MOdificacion titulo 2

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,13 +8,14 @@ class Landing extends Component {
         <div className="container">
           <div className="row justify-content-center text-center">
             <div className="col-md-10 col-lg-6">
-              <h1 className="display-5 text-primary">
+              <h1 className="display-3 text-dark">
                 Bienvenido a LTBA Construcciones
               </h1>
               <br />
-              <p className="lead text-info">
-                Una descripción de la empresa o una frase acerca de nosotros
-              </p>
+              <h3 className="lead text-dark">
+                Una descripción de la empresa o una frase
+              </h3>
+              <br />
               <br />
               <br />
               <p className="lead">
